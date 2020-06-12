e5116e6543904d2ad8073b2c6fe828053c47362e
Suraj Singh <surajupes@outlook.com>
2020-06-12 20:11:24 +0200

    initial commit

---
+++ max.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n , max, i, numb;
+	printf("give n numbers");
+	scanf("\n%d", &n);
+	printf("\n%d", n);
+	scanf("%d", &max);
+	for (i=1; i<= n-1;i++)
+	{
+		scanf("%d", &numb);
+		if (numb> max)
+		{
+			max=numb;
+		}
+
+	
+	}
+  	printf("\n MAX=%d ",max); 
+	printf("enter your name");
+	
+
+	return 0;
+}
+
